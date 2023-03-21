5f56ed77df783bb4bfc99fcd420cbb3d52daa9f3
Cihan <cihankovanci@gmail.com>
2023-03-22 01:15:05 +0300

    Added Firebase Auth

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import NavigationComponent from "./src/navigation/Navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <NavigationComponent />;
 }
 
 const styles = StyleSheet.create({
